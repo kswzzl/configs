efc92df06d3f4b662b14c107d5c16ebb154f7e02
kswzzl <1975987+kswzzl@users.noreply.github.com>
2025-04-10 14:35:39 -0400

    fuzzy git branch find/checkout thingy

--- nvim/lua/kevin/git.lua
+++ nvim/lua/kevin/git.lua
@@ -19,7 +19,36 @@ require("gitsigns").setup({
   end,
 })
 
--- Optional: vim-fugitive keybindings
+-- fuzzy remote branch pull function
+local pick_remote_branch = function()
+  local pickers = require("telescope.pickers")
+  local finders = require("telescope.finders")
+  local actions = require("telescope.actions")
+  local action_state = require("telescope.actions.state")
+  local conf = require("telescope.config").values
+
+  -- Get remote branches (strip remotes/origin/)
+  local branches = vim.fn.systemlist("git branch -r | grep -v HEAD | sed 's|origin/||' | sort -u")
+
+  pickers.new({}, {
+    prompt_title = "Remote Git Branches",
+    finder = finders.new_table {
+      results = branches,
+    },
+    sorter = conf.generic_sorter({}),
+    attach_mappings = function(_, map)
+      actions.select_default:replace(function()
+        actions.close()
+        local selection = action_state.get_selected_entry()[1]
+        -- Checkout the remote branch locally
+        vim.cmd("Git checkout -b " .. selection .. " origin/" .. selection)
+      end)
+      return true
+    end,
+  }):find()
+end
+
+-- vim-fugitive keybindings
 vim.keymap.set("n", "<leader>g", ":Git<CR>", { desc = "Open Git UI" })
 vim.keymap.set("n", "<leader>gl", ":Git log<CR>", { desc = "Git log" })
 vim.keymap.set("n", "<leader>gc", ":Git commit<CR>", { desc = "Git commit" })
