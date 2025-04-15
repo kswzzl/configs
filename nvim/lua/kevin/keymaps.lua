2aa3aefbee05977bbdd77ede9eb4abbc2035c5d4
kswzzl <1975987+kswzzl@users.noreply.github.com>
2025-04-15 13:54:16 -0400

    lsp keymap stuff

--- nvim/lua/kevin/keymaps.lua
+++ nvim/lua/kevin/keymaps.lua
@@ -4,8 +4,9 @@ local keymap = vim.keymap
 
 keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
 keymap.set("n", "qq", ":qall<CR>", { desc = "quit all" })
-keymap.set("n", "<leader>bn", ":bnext<CR>", { desc = "Next buffer" })
-keymap.set("n", "<leader>bp", ":bprev<CR>", { desc = "Previous buffer" })
+keymap.set("n", "<leader>bn", ":bnext<CR>", { desc = "next buffer" })
+keymap.set("n", "<leader>bp", ":bprev<CR>", { desc = "previous buffer" })
 --keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>", { desc = "Next buffer" })
 --keymap.set("n", "<S-Tab>", ":BufferLineCyclePrev<CR>", { desc = "Previous buffer" })
-keymap.set("n", "<leader>dm", ":DiffviewOpen master...HEAD<CR>", { desc = "Diff vs master" })
+keymap.set("n", "<leader>dm", ":DiffviewOpen master...HEAD<CR>", { desc = "diff vs master" })
+keymap.set("n", "<leader>dx", ":DiffviewClose<CR>", { desc = "exit diffview" })
--- nvim/lua/kevin/lsp.lua
+++ nvim/lua/kevin/lsp.lua
@@ -1,10 +1,10 @@
--- Mason for managing language servers
+-- Mason: install/manage LSP servers
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = { "pyright" },
+  ensure_installed = { "pyright", "gopls" },
 })
 
--- nvim-cmp (autocomplete)
+-- Autocompletion setup
 local cmp = require("cmp")
 local luasnip = require("luasnip")
 
@@ -18,14 +18,22 @@ cmp.setup({
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<CR>"] = cmp.mapping.confirm({ select = true }),
     ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then luasnip.expand_or_jump()
-      else fallback() end
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
     end, { "i", "s" }),
     ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then luasnip.jump(-1)
-      else fallback() end
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
     end, { "i", "s" }),
   }),
   sources = {
@@ -36,14 +44,36 @@ cmp.setup({
   },
 })
 
--- LSP config for Python
-local lspconfig = require("lspconfig")
+-- Capabilities for LSP <-> completion integration
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
+local lspconfig = require("lspconfig")
 
-lspconfig.pyright.setup({
-  capabilities = capabilities,
-  on_attach = function(_, bufnr)
-    require("lsp_signature").on_attach({}, bufnr)
-  end,
-})
+-- Full on_attach with keymaps and which-key integration
+local on_attach = function(_, bufnr)
+  local opts = { buffer = bufnr, noremap = true, silent = true }
+
+  -- Core navigation
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+  vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+  vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+
+  -- Signature help in insert mode
+  vim.keymap.set("i", "<C-s>", vim.lsp.buf.signature_help, opts)
+
+  -- Leader-based actions
+  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = bufnr, desc = "Rename symbol" })
+  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = bufnr, desc = "Code action" })
+end
+
+-- Setup LSP servers
+local servers = { "pyright", "gopls" }
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
+  })
+end
 
